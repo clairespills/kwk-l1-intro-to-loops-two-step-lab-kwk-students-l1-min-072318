790eaf5de08668c3a38a030d140e0367b60367d3
clairespills <purpleprincess372@gmail.com>
2018-07-25 15:02:10 +0000

    automatically backed up by learn

---
+++ chaii.rb
@@ -0,0 +1,13 @@
+def first_steps
+  loop do
+  puts "Right foot back"
+  sleep(0.5)
+  puts "left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "stop"
+  end
+end
+
+first_steps
